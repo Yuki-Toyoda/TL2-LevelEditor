da936b8293118a79152aae0ccff68436fa086556
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2025-02-10 22:55:50 +0900

    有効、無効フラグの追加

---
+++ disabled.py
@@ -0,0 +1,33 @@
+import bpy
+
+class MYADDON_OT_add_disabled(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_create_object"
+    bl_label = "無効オプション追加"
+    bl_description = "['disabled']有効、無効オプションの追加"
+    # redo, undo 可能オプション
+    bl_optioms = {'REGISTER', 'Undo'}
+
+    def execute(self, context):
+        # 無効オプションの追加
+        context["disabled"] = True
+        
+        # オペレーターの命令終了を通知
+        return {'FINISHED'}
+
+
+class OBJECT_PT_disabled(bpy.types.Panel):
+    bl_idname = "OBJECT_PT_disabled"
+    bl_label = "Disabled"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "object"
+
+    # サブメニューの描画
+    def draw(self, context):
+        # パネルに項目追加
+        if "disabled" in context.object:
+            # 既にプロパティがある場合プロパティを表示
+            self.layout.prop(context.object, '["disabled"]', text=self.bl_label)
+        else:
+            # プロパティがなければプロパティの追加ボタンを表示
+            self.layout.operator(MYADDON_OT_add_disabled.bl_idname)
